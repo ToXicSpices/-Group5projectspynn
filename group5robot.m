41446af9c310cdca8b5f433c01e9c201f9debb72
ToXicSpices <89231598+ToXicSpices@users.noreply.github.com>
2024-10-23 17:16:44 -0700

    Update group5robot.m

--- group5robot.m
+++ group5robot.m
@@ -1,1 +1,19 @@
+reading = brick.TouchPressed(4);
+turn=true;
+while 1
+ if touch
+     touch = false;
+     brick.StopAllMotors('Brake');
+     pause(1);
+     brick.MoveMotor('A', 30);
+     brick.MoveMotor('B', -30);
+     pause(0.6)
+     brick.StopAllMotors('Brake');
+    
+ else
+   touch = brick.TouchPressed(4);
+   brick.MoveMotor('A', -50);
+   brick.MoveMotor('B', -50);
+ end
+end
 
