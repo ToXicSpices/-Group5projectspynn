914415787ccfced6ed2c4b2b8230721ba6f4a608
ToXicSpices <89231598+ToXicSpices@users.noreply.github.com>
2024-10-30 17:17:54 -0700

    Update group5robot.m

--- group5robot.m
+++ group5robot.m
@@ -1,18 +1,28 @@
-reading = brick.TouchPressed(4);
-turn=true;
 while 1
- if touch
-     touch = false;
-     brick.StopAllMotors('Brake');
-     pause(1);
-     brick.MoveMotor('A', 30);
-     brick.MoveMotor('B', -30);
-     pause(0.6)
-     brick.StopAllMotors('Brake');
-    
- else
-   touch = brick.TouchPressed(4);
    brick.MoveMotor('A', -50);
-   brick.MoveMotor('B', -50);
+   brick.MoveMotor('B', -47);
+ touch = brick.TouchPressed(4);
+ 
+ if touch
+     distance = brick.UltrasonicDist(3);
+   if (distance <= 50)
+       pause(0.40);
+       brick.MoveMotor('A', 60);
+       brick.MoveMotor('B', 60);
+       pause(0.40);
+       brick.MoveMotor('A', 30);
+       brick.MoveMotor('B', -30);
+       pause(0.40);
+       distance = 0;
+   else
+     pause(0.40);
+       brick.MoveMotor('A', 60);
+       brick.MoveMotor('B', 60);
+       pause(0.40);
+       brick.MoveMotor('A', -20);
+       brick.MoveMotor('B', 30);
+       pause(0.40);
  end
+ end
+ pause(0.5);
 end
