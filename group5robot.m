ff2b5871b1ba8e23e8c5ffbf410cbcc0a0d55e32
ToXicSpices <89231598+ToXicSpices@users.noreply.github.com>
2024-11-06 17:59:33 -0700

    Update group5robot.m

--- group5robot.m
+++ group5robot.m
@@ -1,29 +1,80 @@
 
 while 1
+  brick.MoveMotor('A', -50);
+  brick.MoveMotor('B', -47.25);
+  color = brick.ColorCode(1);
+  brick.SetColorMode(1, 2);
+  touch = brick.TouchPressed(3);
+  distance = brick.UltrasonicDist(2);
+   if (touch)
+      brick.MoveMotor('A', 60);
+      brick.MoveMotor('B', 60);
+      pause(0.45);
+      brick.MoveMotor('A', 30.5);
+      brick.MoveMotor('B', -28);
+      pause(0.9);
+  end
+  if(distance >= 40)
+      pause(0.8);
+      brick.MoveMotor('A', -30);
+      brick.MoveMotor('B', 30);
+      pause(0.9);
+      brick.MoveMotor('A', -50);
+  brick.MoveMotor('B', -47.25);
+   pause(1.5)   
+  end
+ 
+  if (color == 5)
+   brick.MoveMotor('A', 0);
+   brick.MoveMotor('B', 0);
+   pause(2)
    brick.MoveMotor('A', -50);
    brick.MoveMotor('B', -47.25);
- touch = brick.TouchPressed(4);
-  distance = brick.UltrasonicDist(3);
- if touch
-    
-   if (distance <= 30)
- 
-       brick.MoveMotor('A', 60);
-       brick.MoveMotor('B', 60);
-       pause(0.45);
-       brick.MoveMotor('A', 30.5);
-       brick.MoveMotor('B', -30.5);
-       pause(0.425);
-       
-   else
-
-       brick.MoveMotor('A', 60);
-       brick.MoveMotor('B', 60);
-       pause(0.45);
-       brick.MoveMotor('A', -30.5);
-       brick.MoveMotor('B', 28);
-       pause(0.425);
- end
- end
- pause(0.5);
+   pause(1)
 end
+if(color == 2)
+   brick.MoveMotor('A', 0);
+   brick.MoveMotor('B', 0);
+   pause(0.7)
+   brick.beep();
+   pause(0.7);
+   brick.beep();
+   pause(0.7);
+   brick.MoveMotor('A', -50);
+   brick.MoveMotor('B', -47.25);
+   pause(1)
+  
+end
+if(color == 3)
+   brick.MoveMotor('A', 0);
+   brick.MoveMotor('B', 0);
+   pause(0.7);
+   brick.beep();
+   pause(0.7);
+   brick.beep();
+   pause(0.7);
+   brick.beep();
+   pause(0.7);
+   brick.MoveMotor('A', -50);
+   brick.MoveMotor('B', -47.25);
+   pause(1)
+end
+if(color == 4)
+   brick.MoveMotor('A', 0);
+   brick.MoveMotor('B', 0);
+   pause(0.7);
+   brick.beep();
+   pause(.7);
+   brick.beep();
+   pause(.7);
+   brick.beep();
+   pause(.7);
+   brick.beep();
+   pause(.7);
+   brick.MoveMotor('A', -50);
+   brick.MoveMotor('B', -47.25);
+   pause(1)
+end
+ end
+
+
